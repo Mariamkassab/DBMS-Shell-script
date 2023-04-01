90da84ea4aeb7eb54571a0f422867008f73a9abc
Rania Fahmy <rfahmy@localhost.localdomain>
2023-04-01 19:50:13 +0200

    main minu, create database, list databases

--- firstselection.sh
+++ firstselection.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
+mkdir DataBases 2>> /dev/null
+cd DataBases
+function menu1 {
+    echo Hello Dear,
+    echo kindly choose one option from the list below 
+    echo "1. Create a database "
+    echo "2. List a database "
+    echo "3. Connect to a database "
+    echo "4. Delete a database "
+    echo "5. Exit "
+    read -p "So what is your choice? [1-5]" choice
+    case $choice in
+        1) create_database ;;
+        2) list_databases ;;
+        3) connect_database ;;
+        4) drop_database ;;
+        5) exit_program ;;
+        *) echo "Invalid choice. Please try again." ; menu1 ;;
+    esac
+}
+#create new database
+function create_database {
+    read -p "Enter the database name: " db_name
+    if [[ ! "$db_name" =~ ^[a-zA-Z0-9_]+$ ]]; then
+        echo "Invalid database name. Please use only alphanumeric characters and underscore."
+    elif check_database_exists "$db_name"; then
+        return
+    else
+        mkdir "$db_name"
+        echo "Database created successfully!"
+    fi
+    menu1
+}
 
-echo Hello Dear,
-echo kindly choice one option from the list below 
-echo 
-
- select choice in "create database" "list database" "connect to database" "drop database"
- do
- 
- case $REPLY in 
- 
-1)
-echo kindly type the name if the database
-read $REPLY
-cd ~/Desktop/bashproject/database
-ls database > list
-
-
-;;
-2)
-echo 2
-;;
-3)
-echo 3
-;;
-4)
-echo 4
-;;
-*)
-echo $REPLY in not one of the optiones.
-;;
-
-esac
-done
-
-
-
-
+#list all databases
+function list_databases {
+    echo "List of existing databases:"
+    ls DataBases
+    menu1
+}
